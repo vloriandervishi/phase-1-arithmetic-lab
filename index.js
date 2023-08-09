ee8a017000776dea274d2178540110c42165fc30
vlorian <vlorian.dervishi@gmail.com>
2023-08-09 18:09:51 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-
 // Write your code here
+/**
+ * Multiplies two numbers together.
+ * @param {number} num1 - The first number to multiply.
+ * @param {number} num2 - The second number to multiply.
+ * @returns The product of the two numbers.
+ */
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+/**
+ * Generates a random number between 1 and 5 (inclusive).
+ * @returns {number} The randomly generated number.
+ */
+let random = Math.floor(Math.random() * 5) + 1;
+
+/**
+ * Calculates the modulus of two numbers.
+ * @param {number} num3 - The dividend.
+ * @param {number} num4 - The divisor.
+ * @returns The modulus of num3 divided by num4.
+ */
+let num3 = 4;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+/**
+ * Finds the maximum value among the given numbers.
+ * @param {...number} numbers - The numbers to compare.
+ * @returns The maximum value among the given numbers.
+ */
+let max = Math.max(1, 2, 4, 5, 20);
